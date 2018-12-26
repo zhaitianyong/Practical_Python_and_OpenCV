bfee975ffd8a3e000ecbd40d322b76231810cd12
ml <tyzhai@126.com>
2018-12-26 14:03:54 +0800

    test opencv module

--- Chapter3/test.py
+++ Chapter3/test.py
@@ -4,6 +4,8 @@ import numpy as np
 
 
 if __name__== "__main__":
+    print(cv2.__version__)
     image = cv2.imread("/home/ml/data/lena.jpg")
     cv2.imshow("image", image)
     cv2.waitKey()
+    cv2.destroyAllWindows()
